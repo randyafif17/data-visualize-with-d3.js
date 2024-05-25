5c74be380abd43ec2ea540d9560e042bc3c03b3f
randyafif17 <randyafif17@gmail.com>
2024-05-25 15:23:23 +0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import appleStock, {AppleStock} from "@visx/mock-data/lib/mocks/appleStock";
+import useMeasure from "react-use-measure";
+import { scaleBand, scaleLinear } from "@visx/scale";
+import { Group } from "@visx/group";
+import { AxisLeft, AxisBottom } from "@visx/axis";
+import { Bar } from "@visx/shape";
+import { timeFormat } from "d3-time-format";
+import moment from "moment";
+
+const data = appleStock.slice(0, 10);
+
+const margin = 32;
+
+const defaultWidth = 100;
+const defaultHeight = 100;
+
+const getXValue = (d: AppleStock) => d.date;
+const getYValue = (d: AppleStock) => d.close;
 
 function App() {
+  const [ref, bounds] = useMeasure();
+
+  const width = bounds.width || defaultWidth;
+  const height = bounds.height || defaultHeight;
+
+  const innerWidth = width - margin * 2;
+  const innerHeight = height - margin * 2;
+
+  const xScale = scaleBand<string>({
+    range: [margin, innerWidth],
+    domain: data.map(getXValue),
+    padding: 0.2,
+  });
+
+  const yScale = scaleLinear<number>({
+    range: [innerHeight, margin],
+    domain: [
+      Math.min(...data.map(getYValue)) - 1,
+      Math.max(...data.map(getYValue)) + 1,
+    ],
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <svg 
+    ref={ref} 
+    width="100%" 
+    height="100%"
+    >
+      <Group>
+        {data.map((d) => {
+          const xValue = getXValue(d);
+          const barWidth = xScale.bandwidth();
+          const barHeight = innerHeight - (yScale(getYValue(d)) ?? 0);
+          const barX = xScale(xValue);
+          const barY = innerHeight - barHeight;
+          return (
+            <Bar 
+              key={`bar-${xValue}`}
+              x={barX}
+              y={barY}
+              width={barWidth}
+              height={barHeight}
+              fill="orange"
+            />
+          )
+        })}
+      </Group>
+      <Group>
+        <AxisLeft left={margin} scale={yScale} />
+      </Group>
+      <Group>
+        <AxisBottom top={innerHeight} 
+        scale={xScale} 
+        tickFormat={(d) => moment(d).format('DD MMM YY')}
+        />
+      </Group>
+    </svg>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createGlobalStyle } from "styled-components";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+      "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+      sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+    width: 100%;
+  }
+`;
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
